04b8fb9e05252976940f0080d693b48dfce9f638
abhay-raj19 <rajpootabhay423@gmail.com>
2024-05-29 17:06:07 +0530

    added up a vite
    Signed-off-by: abhay-raj19 <rajpootabhay423@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Aos from "aos";
+import 'aos/dist/aos.css'
 import "./App.css";
 import Header from "./components/Header/Header";
 import Exercises from "./components/UI/Exercises";
